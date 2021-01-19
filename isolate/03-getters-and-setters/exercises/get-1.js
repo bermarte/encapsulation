fc26e03054a430c4aaa807c9454f519081306a99
Bermarte <bernardo.martelli@gmail.com>
2021-01-19 18:50:20 +0100

    getters isolate 3

--- isolate/03-getters-and-setters/exercises/get-1.js
+++ isolate/03-getters-and-setters/exercises/get-1.js
@@ -9,14 +9,16 @@ const obj1 = {
 const obj2 = {
   name: 'obj2',
   beginning: "hi, I'm",
-  get greeting() {},
+  getGreeting: function() {
+    return `${this.beginning} ${this.name}`;
+  },
 };
 
 const obj1Greeting1 = obj1.getGreeting();
 const test1 = obj1Greeting1 === `hi, I'm obj1`;
 console.assert(test1, 'Test 1');
 
-const obj2Greeting1 = _;
+const obj2Greeting1 = obj2.getGreeting();
 const test2 = obj2Greeting1 === `hi, I'm obj2`;
 console.assert(test2, 'Test 2');
 
@@ -29,6 +31,6 @@ console.assert(test3, 'Test 3');
 
 obj2.beginning = 'good bye';
 
-const obj2Greeting2 = _;
+const obj2Greeting2 = obj2.getGreeting();
 const test4 = obj2Greeting2 === `good bye second`;
 console.assert(test4, 'Test 4');
--- isolate/03-getters-and-setters/exercises/get-2.js
+++ isolate/03-getters-and-setters/exercises/get-2.js
@@ -12,13 +12,18 @@ const obj2 = {
   numbers: [12, 4, 9, 36, 7, 0, -2],
   modulo: 3,
   // write a getter that matches the behavior of .getZeroRemainders
+  get getZeroRemainders() {
+    return this.numbers.filter(x => {
+      return x % this.modulo === 0
+    });
+  }
 };
 
-const obj1ZeroRemainders = obj1._;
+const obj1ZeroRemainders = obj1.getZeroRemainders();
 const test1a = deepCompare(obj1ZeroRemainders, [12, 9, 36, 0]);
 console.assert(test1a, 'Test 1 a');
 
-const obj2ZeroRemainders = obj2._;
+const obj2ZeroRemainders = obj2.getZeroRemainders;
 const test1b = deepCompare(obj2ZeroRemainders, [12, 9, 36, 0]);
 console.assert(test1b, 'Test 1 b');
 
@@ -26,11 +31,11 @@ console.assert(test1b, 'Test 1 b');
 obj1.modulo = 6;
 obj2.modulo = 6;
 
-const obj1ZeroRemainders2 = _;
+const obj1ZeroRemainders2 = obj1.getZeroRemainders();
 const test2a = deepCompare(obj1ZeroRemainders2, [12, 36, 0]);
 console.assert(test2a, 'Test 2 b');
 
-const obj2ZeroRemainders2 = _;
+const obj2ZeroRemainders2 = obj2.getZeroRemainders;
 const test2b = deepCompare(obj2ZeroRemainders2, [12, 36, 0]);
 console.assert(test2b, 'Test 2 b');
 
